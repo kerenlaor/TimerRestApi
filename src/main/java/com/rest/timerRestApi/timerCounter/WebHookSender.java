1c458c3004337505bf77ddab7bcbf2d3dfe1db0c
kerenLaor <keren199622@gmail.com>
2024-03-04 06:39:53 +0200

    send webhook with http

--- src/main/java/com/rest/timerRestApi/timerCounter/WebHookSender.java
+++ src/main/java/com/rest/timerRestApi/timerCounter/WebHookSender.java
@@ -1,37 +1,46 @@
 package com.rest.timerRestApi.timerCounter;
 
+import com.google.gson.Gson;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.net.ssl.SSLException;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import com.schemas.modelRest.WebhookTimerRequest;
+import com.schemas.modelRest.TimerWebhookResponse;
 
 public class WebHookSender {
     Logger logger = LoggerFactory.getLogger(WebHookSender.class);
     public void sendWebhook(String webhookUrl) {
         try {
-            URL url = new URL(webhookUrl);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            Gson gson = new Gson();
+            HttpClient client = HttpClient.newHttpClient();
+            WebhookTimerRequest webhookTimerRequest = new WebhookTimerRequest().message("notify that timer finished");
+            String message = gson.toJson(webhookTimerRequest);
+            logger.warn("json message to send is : {}", message);
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(webhookUrl))
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(message))
+                    .build();
 
-            // Set request method and headers
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type", "application/json");
 
-            // Enable output and send empty body
-            connection.setDoOutput(true);
-            OutputStream outputStream = connection.getOutputStream();
-            outputStream.write("".getBytes());
-            outputStream.flush();
-            outputStream.close();
-
-            int responseCode = connection.getResponseCode();
-            logger.warn("webhook send, Response Code is {} ", responseCode);
-
-            // Close connection
-            connection.disconnect();
-        } catch (Exception e) {
-            e.printStackTrace();
+            //logger.warn("request body is : {}", HttpRequest.BodyPublishers.ofString(message));
+           // logger.warn("");
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (SSLException e) {
+            logger.error("exception is: {}", e.getMessage());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 }
